d7504eee4836b40e9ade3b35c139eaff6af322a1
Anurag Bakode <anuragb@trainee.nrifintech.com>
2023-07-22 20:37:49 +0530

    Added cors policy

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express()
 app.use(cookieParser());
 app.use(morgan('dev'))
 app.use(express.json())
-app.use(cors({ origin: true, credentials: true }));
+app.use("*", cors({ origin: true, credentials: true }));
 
 
 app.get('/', verifyAccessToken, async (req, res, next) => {
